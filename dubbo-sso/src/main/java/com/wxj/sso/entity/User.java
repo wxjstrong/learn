9918ec5cea36814e1064fb92bfd702eb196fb1ec
lihongchao <1553199793@qq.com>
2017-08-28 18:03:55 +0800

    新建一个实体类

---
+++ dubbo-sso/src/main/java/com/wxj/sso/entity/User.java
@@ -0,0 +1,62 @@
+package com.wxj.sso.entity;
+
+public class User {
+     String userName;
+     String password;
+     String nickName;
+     Boolean gender;
+     String idCard;
+     String phoneNum;
+     String address;
+     String eMail;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public Boolean getGender() {
+		return gender;
+	}
+	public void setGender(Boolean gender) {
+		this.gender = gender;
+	}
+	public String getIdCard() {
+		return idCard;
+	}
+	public void setIdCard(String idCard) {
+		this.idCard = idCard;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	
+	
+}
