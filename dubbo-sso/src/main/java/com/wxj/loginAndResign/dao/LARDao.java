c0653112a78770f5fcc82318a16b6b058feeff15
lihongchao <1553199793@qq.com>
2017-09-07 17:39:35 +0800

    给dao添加注解

--- dubbo-sso/src/main/java/com/wxj/loginAndResign/dao/LARDao.java
+++ dubbo-sso/src/main/java/com/wxj/loginAndResign/dao/LARDao.java
@@ -1,11 +1,11 @@
 package com.wxj.loginAndResign.dao;
 
 import java.util.Map;
-
 import org.springframework.stereotype.Repository;
 
 import com.wxj.loginAndResign.entity.User;
 
+@Repository
 public interface LARDao {
 
 	int add(User user);
